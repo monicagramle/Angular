186009c62a42640b0ad884afee8520e35cede1cf
Monica Gramle <monicagramle234@gmail.com>
2022-08-03 08:35:23 +0530

    code fixes

---
+++ angular-form/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  onSubmit(form: NgForm) {
+    console.log('Your form data : ', form.value);
+}
+
+}
